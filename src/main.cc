9415927e6b1a49dd36a18b172bd4b3d79bbe3e44
Bruel Nicolas <bruel.nicolas85@gmail.com>
2016-04-19 21:49:36 +0200

    Added benchmark and fix child process

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/wait.h>
 
 #include "helpers/Logger.hh"
 #include "socket/TCPSocket.hh"
@@ -29,23 +30,22 @@ int main(int argc, char *argv[]) {
     client = socket.acquire();
 
     if (socket.receive(client)) {
-      pid_t pid = fork();
 
-      if(pid < 0){
-        perror("ERROR on fork");
-      }
-
+      pid_t pid;
+      pid = fork();
+      int status;
+      
       if(pid == 0){
         response = socket.getRessource(); // @todo refactor -> déplacer cette méthode dans une classe serveur, ce n'est pas le rôle du socket !
         socket.deliver(client, response);
-        atexit;
+        break;
+      } else {
+        waitpid(pid, &status, WUNTRACED);
       }
       socket.disconnect(client);
     }
-
   }
 
   socket.quit();
-
   return EXIT_SUCCESS;
 }
